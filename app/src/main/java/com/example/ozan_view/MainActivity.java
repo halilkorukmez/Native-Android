be535f9dbed70a734d2b77156cc709b64ddfc336
halilkorukmez <halilkorukmez@clebracode.com>
2021-10-13 03:19:15 +0300

    aaa

---
+++ app/src/main/java/com/example/ozan_view/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ozan_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity  {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.error_page);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
